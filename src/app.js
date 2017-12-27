79a1e0372c55cb2b088e1d0535b9e87e5a1c6222
George Higbie <gehigbie@gmail.com>
2017-12-27 19:01:50 +0000

    Add functions to js file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
-const count = 0;
-const increment = () =>{
+let count = 0;
+const increment = () => {
     count++;
-}
+    renderCounterApp();
+};
+const decrement = () => {
+    count > 0 ? count-- : count = 0;
+    renderCounterApp();
+};
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+};
 
 const template = (
     <div>
@@ -13,5 +22,16 @@ const template = (
 );
 
 const renderCounterApp = () => {
-    
+    const template = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button className="btn btn-default" id="increment" onClick={increment}></button>
+            <button className="btn btn-default" id="decrement" onClick={decrement}></button>
+            <button className="btn btn-default" id="reset" onClick={reset}></button>
+        </div>
+    );
+    const appRoot = document.getElementById('react-container');
+    ReactDOM.render(template, appRoot);
 };
+
+renderCounterApp();
