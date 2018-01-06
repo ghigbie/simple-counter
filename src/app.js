4de98a30ea8b2c4976ef0d980ffab13a9ce2a533
George Higbie <gehigbie@gmail.com>
2018-01-06 03:14:11 +0000

    Modify app.js to hold react component

--- src/app.js
+++ src/app.js
@@ -1,28 +1,46 @@
-let count = 0;
-const increment = () => {
-    count++;
-    renderCounterApp();
-};
-const decrement = () => {
-    count > 0 ? count-- : count = 0;
-    renderCounterApp();
-};
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
+const appRoot = document.getElementById('react-container');
 
-const renderCounterApp = () => {
-    const template = (
-        <div className="container">
-            <h1>Count: {count}</h1>
-            <button className="btn btn-default" id="increment" onClick={increment}>+1</button>
-            <button className="btn btn-default" id="decrement" onClick={decrement}>-1</button>
-            <button className="btn btn-default" id="reset" onClick={reset}>Reset</button>
-        </div>
-    );
-    const appRoot = document.getElementById('react-container');
-    ReactDOM.render(template, appRoot);
-};
+class Counter extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Count: </h1>
+                <button className="btn btn-default" id="increment">+1</button>
+                <button className="btn btn-default" id="decrement">-1</button>
+                <button className="btn btn-danger" id="reset">Reset</button>
+            </div>
+        );
+    }
+}
 
-renderCounterApp();
+ReactDOM.render(<Counter />, appRoot);
+
+
+// let count = 0;
+// const increment = () => {
+//     count++;
+//     renderCounterApp();
+// };
+// const decrement = () => {
+//     count > 0 ? count-- : count = 0;
+//     renderCounterApp();
+// };
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+// };
+
+// const renderCounterApp = () => {
+//     const template = (
+//         <div className="container">
+//             <h1>Count: {count}</h1>
+//             <button className="btn btn-default" id="increment" onClick={increment}>+1</button>
+//             <button className="btn btn-default" id="decrement" onClick={decrement}>-1</button>
+//             <button className="btn btn-default" id="reset" onClick={reset}>Reset</button>
+//         </div>
+//     );
+//     const appRoot = document.getElementById('react-container');
+//     ReactDOM.render(template, appRoot);
+// };
+
+// renderCounterApp();
