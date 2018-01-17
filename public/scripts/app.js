141d151265a5cf03b44f46c27b10e5c8c9737b25
George Higbie <gehigbie@gmail.com>
2018-01-17 16:26:04 +0000

    Add default props for counter component

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,7 +20,7 @@ var Counter = function (_React$Component) {
         _this.handleDecrement = _this.handleDecrement.bind(_this);
         _this.handleReset = _this.handleReset.bind(_this);
         _this.state = {
-            count: 0
+            count: props.count
         };
         return _this;
     }
@@ -97,73 +97,9 @@ var Counter = function (_React$Component) {
     return Counter;
 }(React.Component);
 
+Counter.defaultProps = {
+    count: 0
+};
+
 var appRoot = document.getElementById('react-container');
 ReactDOM.render(React.createElement(Counter, null), appRoot);
-
-// const appRoot = document.getElementById('react-container');
-
-// class Counter extends React.Component{
-//     constructor(props){
-//         super(props);
-//         this.handleIncrement = this.handleIncrement.bind(this);
-//         this.handleDecrement = this.handleDecrement.bind(this);
-//         this.handleReset = this.handleReset.bind(this);
-//         this.state = {
-//             count: props.count
-//         };
-//     }
-
-//     handleIncrement(){
-//         this.setState((prevState) => {
-//           return{
-//                 count: prevState.count +1
-//           }; 
-//         });
-//     }
-
-//     handleDecrement(){
-//         if(this.state.count > 0){
-//             this.setState((prevState) => {
-//                 return{
-//                     count: prevState.count -1
-//                 };
-//             });
-//         }
-//     }
-
-//     handleReset(){
-//         this.state.count = 0;
-//         this.setState(() =>{
-//           return{
-//               count: 0
-//           };  
-//         });
-//     }
-
-//     render(){
-//         return(
-//             <div className="container">
-//                 <h1>Count: {this.state.count}</h1>
-//                 <button className="btn btn-default" 
-//                         id="increment"
-//                         onClick={this.handleIncrement}>+1</button>
-//                 <button className="btn btn-default" 
-//                         id="decrement"
-//                         onClick={this.handleDecrement}>-1</button>
-//                 <button className="btn btn-danger" 
-//                         id="reset"
-//                         onClick={this.handleReset}>Reset</button>
-//             </div>
-//         );
-//     }
-// }
-
-// Counter.defaultProps = {
-//     count: 0
-// };
-
-// const renderApp = () => {
-//     ReactDOM.render(<Counter />, appRoot);
-// };
-
-// renderApp();
